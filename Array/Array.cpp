745bccc61f7fd6711759c84885603fc35d2a7b9d
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-01-03 01:04:18 +0530

    Update Array.cpp

--- Array/Array.cpp
+++ Array/Array.cpp
@@ -37,28 +37,7 @@ arr[0]//=>value at base address +(0*4(int size is 4 bytes))
     arr.capacity();//capacity means the number of elements that can be stored 
   //checking if empty
     arr.empty();//=>true or false
-  // Unique element question
-    // example:[1,2,1,2,3,4,4]=>3 because it is the only element which is unique
-    int findUnique(vector<int> arr){
-      int ans=0;
-      for(int i=0;i<arr.size();i++){
-        ans=ans^arr[i];
-      }
-      return ans;
-    }
-    int main(){
-  
-      int n;
-      cout<<"enter the size of array"<<endl;
-      cin>>n;
-      vector<int> arr(n);
-      cout<<"enter the elements"<<endl;
-      for(int i=0;i<arr.size();i++){
-        cin>>arr[i];
-      }
-      int uniqueElement =findUnique(arr);
-      cout<<uniqueElement;
-    }
+ 
   //
     
 
