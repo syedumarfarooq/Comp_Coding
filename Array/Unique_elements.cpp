20a0f391047a6edd1717cf1620b59d28654e2e9a
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-01-04 18:51:53 +0530

    Update Unique_elements.cpp

--- Array/Unique_elements.cpp
+++ Array/Unique_elements.cpp
@@ -3,7 +3,7 @@
     int findUnique(vector<int> arr){
       int ans=0;
       for(int i=0;i<arr.size();i++){
-        ans=ans^arr[i];
+        ans=ans^arr[i];//xor
       }
       return ans;
     }
