99dc9218dd38a018cfd82c7f4d51f54207200aa8
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-01-25 19:23:43 +0530

    Update Searching_&_Sorting.cpp

--- Searching & Sorting/Searching_&_Sorting.cpp
+++ Searching & Sorting/Searching_&_Sorting.cpp
@@ -85,4 +85,6 @@ int main() {
 // 1.Classic question(lower bound,upper bound,pivot,peak,search in rotated sorted,total occurence,square root etc)
 // 2.Search space question(finding answers in search space)(aggressive cow etc)
 // 3.Index Observation question(finding odd occurence in an array)
-
+//CLASS 4
+Sorting Techniques
+i)Selection sort
