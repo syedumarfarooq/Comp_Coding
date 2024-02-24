30d75b77bb84f96fd6bb5fe67da9425cd9abad8b
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-02-24 21:12:25 +0530

    Update K_diff_Pairs_in_an_array.cpp

--- Searching & Sorting/K_diff_Pairs_in_an_array.cpp
+++ Searching & Sorting/K_diff_Pairs_in_an_array.cpp
@@ -31,21 +31,25 @@
 //   }
 // }
 // 2)two pointer approach
-// int findPairs(vector<int>& nums, int k) {
-//         sort(nums.begin(),nums.end());
-//         int i=0,j=1;
-//         while(j<nums.size()){
-//             int diff=nums[j]-nums[i];
-//             if(diff==k){
-//                 cout<<nums[i]<<" "<<nums[j];
-//                 i++,j++;
-//             }
-//             else if(diff<k){
-//                 j++;
-//             }
-//             else{
-//                 i++;
-//             }
-//         }
-//         return 0;
-//     }
+//  int findPairs(vector<int>& nums, int k) {
+    //     sort(nums.begin(),nums.end());
+    //     set<pair<int,int>> ans;
+    //     int i=0,j=1;
+    //     while(j<nums.size()){
+    //         int diff=nums[j]-nums[i];
+    //         if(diff==k){
+    //             ans.insert({nums[i],nums[j]});
+    //             i++,j++;
+    //         }
+    //         else if(diff<k){
+    //             j++;
+    //         }
+    //         else{
+    //             i++;
+    //         }
+    //         if(i==j){
+    //             j++;
+    //         }
+    //     }
+    //     return ans.size();
+    // }
