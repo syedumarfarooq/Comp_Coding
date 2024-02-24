f963dfba7b891d7d416596ac703823f3891c2230
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-02-24 22:41:20 +0530

    Update k_closest_element.cpp

--- Searching & Sorting/k_closest_element.cpp
+++ Searching & Sorting/k_closest_element.cpp
@@ -15,3 +15,25 @@
 
 // Input: arr = [1,2,3,4,5], k = 4, x = -1
 // Output: [1,2,3,4]
+// 1)two pointer approach
+// class Solution {
+// public:
+//     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+//         int l=0,h=arr.size()-1;
+//         while((h-l)>=k){
+//             if(abs(arr[l]-x)>abs(arr[h]-x)){
+//                 l++;
+//             }else{
+//                 h--;
+//             }
+//         }
+//         vector<int> ans;             
+//         for(int i=l;i<=h;i++){
+//             ans.push_back(arr[i]);
+//         }
+//         return ans;
+//     }
+// };
+// Time complexity:we are reducing the array from n to k so it takes n-k comparison
+//                 O(n-k)
+// Space complexity:we have used O(k) for ans
