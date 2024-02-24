00f03549f7b64136ef98f5ff5054f8da4f0bdfcb
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-02-24 23:01:22 +0530

    Update k_closest_element.cpp

--- Searching & Sorting/k_closest_element.cpp
+++ Searching & Sorting/k_closest_element.cpp
@@ -37,3 +37,43 @@
 // Time complexity:we are reducing the array from n to k so it takes n-k comparison
 //                 O(n-k)
 // Space complexity:we have used O(k) for ans
+// 2)binary search+ two pointer
+// class Solution {
+// public:
+//     int bn(vector<int>& nums,int k){
+//         int s=0,e=nums.size()-1;
+//         int ans=e;
+//         while(s<=e){
+//             int mid=s+(e-s)/2;
+//             if(nums[mid]>=k){
+//                 ans=mid;
+//                 e=mid-1;
+                
+//             }else if(nums[mid]<k){
+//                 s=mid+1;
+//             }else{
+//                 e=mid-1;
+//             }
+//         }
+//         return ans;
+//     }
+//     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+//         int h=bn(arr,x);
+//         int l=h-1;
+//         while((h-l)<=k){
+//             if(l<0){
+//                 h++;
+//             }
+//             else if(h>=arr.size()){
+//                 l--;
+//             }
+//             else if((x-arr[l])>(arr[h]-x)){
+//                 h++;
+//             }
+//             else{
+//                 l--;
+//             }
+//         }
+//         return vector<int>(arr.begin()+l+1,arr.begin()+h);
+//     }
+// };
