8d920630153839daa2d887183031e532471f8ac6
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-02-27 00:22:44 +0530

    Update Exponential_or_Unbounded_search.cpp

--- Searching & Sorting/Exponential_or_Unbounded_search.cpp
+++ Searching & Sorting/Exponential_or_Unbounded_search.cpp
@@ -9,5 +9,8 @@ int exposearch(int a[],int n,int target){
   }
 return binarysearch(a,i/2`(start position)`,min(i,n-1)`(if i goes outside the n it takes n)`,target);
 }
-// Time complexity:O(logm)   where m is till where i goes for example if the i goes till 16 index then m is 16
+// Time complexity:O(log(2^logm-1))or O(log(2^logm/2))
+// example:2^4-2^3=>2^log₂16-2^log₂16-1=>2^log4(as base 2 and 2 from 2^4=16 will be cancelled)=>2^logm-2^logm-1
+//         =>2^logm(1-2^-1)
+//         =>O(logn)=>O(log(2^logm-1))
 
