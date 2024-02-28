d2c9d470a69ec76600ca4e9c607f217f70c35796
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-02-28 21:23:19 +0530

    Update 1CharArray_&_String.cpp

--- CharArray & Strings/1CharArray_&_String.cpp
+++ CharArray & Strings/1CharArray_&_String.cpp
@@ -1,7 +1,9 @@
 // Class I
 // CHAR ARRAY
-//   char name[100];
-//   cin>>name;
-//   cin>>name[2];//this takes input of single character
+  char name[100];
+  cin>>name;
+  cin>>name[2];//this takes input of single character
 // NOTE:by default at the end of char array an null character is placed "\O" is discribes string termination
-
+  #include <string.h>
+  cin.getline(name,50);//it is used to get input of char where there are spaces included for example :name is umar syed it will take in just umar
+  // but if you use getline it takes input as umar syed
