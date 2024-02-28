64777d7fe97deadf30457fd642ee0d800c556516
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-02-28 21:16:08 +0530

    Update 1CharArray_&_String.cpp

--- CharArray & Strings/1CharArray_&_String.cpp
+++ CharArray & Strings/1CharArray_&_String.cpp
@@ -1,1 +1,7 @@
-d
+// Class I
+// CHAR ARRAY
+//   char name[100];
+//   cin>>name;
+//   cin>>name[2];//this takes input of single character
+// NOTE:by default at the end of char array an null character is placed "\O" is discribes string termination
+
