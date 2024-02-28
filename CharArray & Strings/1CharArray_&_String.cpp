65e92099ac32bba631c612f9834181b3681c1fbd
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-02-28 21:34:33 +0530

    Update 1CharArray_&_String.cpp

--- CharArray & Strings/1CharArray_&_String.cpp
+++ CharArray & Strings/1CharArray_&_String.cpp
@@ -3,7 +3,31 @@
   char name[100];
   cin>>name;
   cin>>name[2];//this takes input of single character
-// NOTE:by default at the end of char array an null character is placed "\O" is discribes string termination
+// NOTE:by default at the end of char array an null character is placed "\0" is discribes string termination
   #include <string.h>
   cin.getline(name,50);//it is used to get input of char where there are spaces included for example :name is umar syed it will take in just umar
   // but if you use getline it takes input as umar syed
+  // Question 1
+  // #To find length of char array
+  int getLength(char name[]){
+    int length=0,i=0;
+    while(name[i]!='\0'){
+        length++;
+        i++;
+    }
+  return length;
+  }
+  //Predefined function 
+  #include <string.h>
+  strlen(name);
+  // Question 2
+  // # Reverse a string
+  int reverseCharArray(char name[]){
+    int i=0;
+    int n=getLength(name);
+    int j=n-1;
+    while(i<j){
+      swap(name[i],name[j]);
+      i++,j--;
+    }
+
