ac3063cb01f06d3cc4947ed80fab89d1fb743342
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-03-03 10:35:09 +0530

    Update Valid_Anagram.cpp

--- 3.CharArray & Strings/Valid_Anagram.cpp
+++ 3.CharArray & Strings/Valid_Anagram.cpp
@@ -23,3 +23,22 @@
 //         return true;
 //     }
 // };
+// 2)
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int freqTable[256]={0};//256 because the ascii table range is 256
+        for(int i=0;i<s.size();i++){
+            freqTable[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++){
+            freqTable[t[i]]--;
+        }
+        for(int i=0;i<256;i++){
+            if(freqTable[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
