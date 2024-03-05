84b7f4a042843dd78f4ef3fc64ad1e9f4e5f81b0
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-03-05 10:01:39 +0530

    Update !Pointer.cpp

--- 4.Pointers/!Pointer.cpp
+++ 4.Pointers/!Pointer.cpp
@@ -22,15 +22,23 @@
     int* ptr=&a;
     int* secondptr=ptr;
 // CLASS 2
-    int arr[4]={1,2,3,4}
-    cout<<arr;//this gives the base address of arr            }
-    cout<<&arr;//this also gives the address of  arr          }there address are all same
-    cout<<&arr[0]//this gives the address of arr              }
-    cout<<*arr;//this gives 1 as output 
-    cout<<*(arr+1);//=>2
-    cout<<*(arr+i)<<arr[i]<<i[arr];//all are same 
-    int* p=arr;
-    arr=arr+1;//ERROR
-    p=p+1;//works fine
+    //ARRAY
+        int arr[4]={1,2,3,4}
+        cout<<arr;//this gives the base address of arr            }
+        cout<<&arr;//this also gives the address of  arr          }there address are all same
+        cout<<&arr[0]//this gives the address of arr              }
+        cout<<*arr;//this gives 1 as output 
+        cout<<*(arr+1);//=>2
+        cout<<*(arr+i)<<arr[i]<<i[arr];//all are same 
+        int* p=arr;
+        arr=arr+1;//ERROR arr base address cannot be changed because constant pointer
+        p=p+1;//works fine
+    //CHAR ARRAY
+        //cout implementation is different for char array
+        char chr[10]="umar";
+        char* c=chr;
+        cout<<*c;//=>U
+        cout<<c;//=>THIS WILL PRINT UMAR
+        //it prints out the entire string of that address
 
 
