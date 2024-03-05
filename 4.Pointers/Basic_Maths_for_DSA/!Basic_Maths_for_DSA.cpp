ec987eb7e97aa80a237b2b5f799aad1abf0a4990
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-03-05 16:31:32 +0530

    Update !Basic_Maths_for_DSA.cpp

--- 4.Pointers/Basic_Maths_for_DSA/!Basic_Maths_for_DSA.cpp
+++ 4.Pointers/Basic_Maths_for_DSA/!Basic_Maths_for_DSA.cpp
@@ -8,3 +8,19 @@
     // 2. a % M - b% M= (a - b) %M
     // 3. ((а % М) % М) % М= a % М
     // 4. а % M*b% M=(a*b) %M
+//Fast exponentation
+    //a^b 
+    //if b is even=>(a^b/2)^2
+    //if b is odd=>(a^b/2)^2.a
+    // eg:2^5=>(2^4).2=>(2*2*2*2)*2
+    int fastexponentation(int a,int b){
+        int ans=1;
+        while(b>0){
+            if(b&1){//as odd numbers have 1 in the last bit and 1&1 is 1 
+                ans*=a;
+            }
+            a=a*a;
+            b>>=1;
+        }
+        return ans;
+    }
