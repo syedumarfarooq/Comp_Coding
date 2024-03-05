57684b68c89d7ad16e861a35682d0d0a2ee2d2c4
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-03-05 08:35:16 +0530

    Update !Pointer.cpp

--- 4.Pointers/!Pointer.cpp
+++ 4.Pointers/!Pointer.cpp
@@ -1,1 +1,6 @@
-
+// CLASS 1
+  int a=5;
+  char ch='a';
+  cout<<&a;//where & is the address of operator and it is stored in symbol table
+  int* ptr=&a;//ptr is a pointer to integer data
+  char* p=&ch;//p is a pointer to char data
