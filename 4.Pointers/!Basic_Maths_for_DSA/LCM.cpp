b90a019625c180cf2af4c784227aada4bd9e713f
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-03-05 16:13:54 +0530

    Update and rename LCM.cpp to !Basic_Maths_for_DSA.cpp

--- 4.Pointers/!Basic_Maths_for_DSA/LCM.cpp
+++
@@ -1,3 +0,0 @@
-//Formula:lcm(a,b)*gcd(a,b)=a*b
-// LCM(a,b)=a*b/GCD(a,b) 
-//gcd code is written in another file
---
+++ 4.Pointers/Basic_Maths_for_DSA/!Basic_Maths_for_DSA.cpp
@@ -0,0 +1,10 @@
+//LCM
+    // Formula:lcm(a,b)*gcd(a,b)=a*b
+    // LCM(a,b)=a*b/GCD(a,b) 
+    // gcd code is written in another file
+//Modulo arithmetic
+    // Generally, to avoid overflow while storing Integer we do modulo with a Large number.
+    // 1. (a + b) % M=a % M+ % M
+    // 2. a % M - b% M= (a - b) %M
+    // 3. ((а % М) % М) % М= a % М
+    // 4. а % M*b% M=(a*b) %M
