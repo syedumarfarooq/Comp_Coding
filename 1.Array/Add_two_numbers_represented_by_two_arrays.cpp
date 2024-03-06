75e9662e5f291b6b39a4df3b06213a99d98923d3
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-03-06 08:05:36 +0530

    Update Add_two_numbers_represented_by_two_arrays.cpp

--- 1.Array/Add_two_numbers_represented_by_two_arrays.cpp
+++ 1.Array/Add_two_numbers_represented_by_two_arrays.cpp
@@ -59,26 +59,3 @@ class Solution{
     }
 };
 
-//{ Driver Code Starts.
-int main() {
-    int t;
-	cin>>t;
-	while(t--){
-	    int n;
-	    cin>>n;
-	    int a[n];
-	    for(int i=0;i<n;i++){
-	        cin>>a[i];
-	    }
-	    int m;
-	    cin>>m;
-	    int b[m];
-	    for(int i=0;i<m;i++){
-	        cin>>b[i];
-	    }
-	    Solution ob;
-	    cout << ob.calc_Sum(a,n,b,m) << endl;
-	}
-	return 0;
-}
-// } Driver Code Ends
