e448b889b45c6b11e3693b7cb0f9dcdc53f0026a
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-03-18 18:07:58 +0530

    Update !Recursion&Backtracing.cpp

--- 6.Recursion&Backtracking/!Recursion&Backtracing.cpp
+++ 6.Recursion&Backtracking/!Recursion&Backtracing.cpp
@@ -1,22 +1,46 @@
 //Time and space Complexity of Recursive Solution
-// Time Complexity:O(n)
-// eg:
-void printArray(int all, int n){
-  if(n== 0) return;  }
-  cout<<*a<<" ";     }takes k time to process both combined 
-  printArray(a+1, n-1);
-｝
-// T(n)=k+T(n-1)
-// T(n-1)=k+T(n-2)
-// T(n-2)=k+T(n-3)
-// T(n-3)=k+T(n-4)
-//   .
-//   .
-//   .
-//   .
-// T(1)=k+T(0)
-// T(0)=k1       }as k1 is the time to process only the if(n==0) as it does not process cout line so different process time
-// -----------------
-// T(n)=nK+k1 }   as t(n-1),T(n-2)... will all get cancelled
-// =>O(nK+k1)
-// =>O(n)
+  //1) Time Complexity:O(n)
+      // eg:
+      void printArray(int all, int n){
+        if(n== 0) return;  //}
+        cout<<*a<<" ";     //}takes k time to process both combined 
+        printArray(a+1, n-1);
+      ｝
+      // T(n)=k+T(n-1)
+      // T(n-1)=k+T(n-2)
+      // T(n-2)=k+T(n-3)
+      // T(n-3)=k+T(n-4)
+      //   .
+      //   .
+      //   .
+      //   .
+      // T(1)=k+T(0)
+      // T(0)=k1       }as k1 is the time to process only the if(n==0) as it does not process cout line so different process time
+      // -----------------
+      // T(n)=nK+k1 }   as t(n-1),T(n-2)... will all get cancelled
+      // =>O(nK+k1)
+      // =>O(n)
+  //Space Complexity:O((n+1)*m)=O(n)}where is m is constant space
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+  // 2)Time Complexity:O(n)
+      // eg:
+      int fact(int n)
+        if (n == 1)  //}takes k time to process
+        return 1;
+        return n * fact(n - 1);
+        ｝
+      // T(n)=k+T(n-1)
+      // T(n-1)=k+T(n-2)
+      // T(n-2)=k+T(n-3)
+      // T(n-3)=k+T(n-4)
+      //   .
+      //   .
+      //   .
+      //   .
+      // T(1)=k+T(0)
+      // T(0)=k      
+      // -----------------
+      // T(n)=nK }   as t(n-1),T(n-2)... will all get cancelled
+      // =>O(nK)
+      // =>O(n)
+  //Space Complexity:O(n*m)=O(n)}where is m is constant space
