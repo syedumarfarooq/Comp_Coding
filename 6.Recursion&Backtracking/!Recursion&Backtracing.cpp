c0a0cb06977761ae814b90b9c5004c35670df98a
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-03-18 17:57:24 +0530

    Update !Recursion&Backtracing.cpp

--- 6.Recursion&Backtracking/!Recursion&Backtracing.cpp
+++ 6.Recursion&Backtracking/!Recursion&Backtracing.cpp
@@ -1,1 +1,22 @@
-hi
+//Time and space Complexity of Recursive Solution
+// Time Complexity:O(n)
+// eg:
+void printArray(int all, int n){
+  if(n== 0) return;  }
+  cout<<*a<<" ";     }takes k time to process both combined 
+  printArray(a+1, n-1);
+｝
+// T(n)=k+T(n-1)
+// T(n-1)=k+T(n-2)
+// T(n-2)=k+T(n-3)
+// T(n-3)=k+T(n-4)
+//   .
+//   .
+//   .
+//   .
+// T(1)=k+T(0)
+// T(0)=k1       }as k1 is the time to process only the if(n==0) as it does not process cout line so different process time
+// -----------------
+// T(n)=nK+k1 }   as t(n-1),T(n-2)... will all get cancelled
+// =>O(nK+k1)
+// =>O(n)
