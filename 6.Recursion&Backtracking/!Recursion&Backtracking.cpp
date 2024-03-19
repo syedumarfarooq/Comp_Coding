24da13972e64a9f034052a777ef8ebcd3eaedc9c
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-03-20 00:53:44 +0530

    Update !Recursion&Backtracking.cpp

--- 6.Recursion&Backtracking/!Recursion&Backtracking.cpp
+++ 6.Recursion&Backtracking/!Recursion&Backtracking.cpp
@@ -1,1 +1,18 @@
-h
+// CLASS 1
+// // RECURSION: when a function call itself
+//       When is recursion used
+//         when a bigger problem solution depends on small and same type of problem solution
+    // --Recursive code conditions:
+    // 1.Base condition
+    // 2.Recursive relation// (f(n)=n+f(n-1) i.e bigger problem depends on smaller problem)
+    // 3.processing(optional)//(printing or calcutaion)
+    // -----Head Recursion
+    // 1.Base condition
+    // 2.Recursive relation// (f(n)=n+f(n-1) i.e bigger problem depends on smaller problem)
+    // 3.processing(optional)//(printing or calcutaion)
+    // -----Tail Recursion
+    // 1.Base condition
+    // 2.Recursive relation// (f(n)=n+f(n-1) i.e bigger problem depends on smaller problem)
+    // 3.processing(optional)//(printing or calcutaion)
+    // --Magical line to ace recursion
+       // solve 1 case and recursion will take care of other cases
