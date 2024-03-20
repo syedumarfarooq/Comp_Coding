fbbdd817d3a2bed6a0fce294bf122d728a335c06
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-03-21 02:07:57 +0530

    Update !Recursion&Backtracking.cpp

--- 6.Recursion&Backtracking/!Recursion&Backtracking.cpp
+++ 6.Recursion&Backtracking/!Recursion&Backtracking.cpp
@@ -19,3 +19,17 @@
 _________________IMP_______________    
 WHENEVER YOU PASS A VARIABLE IN A FUNCTION AND U WANT TO STORE THAT DATA PASS IT BY REFERENCE&
 //if it is not passed by reference it will create a new variable of same name at different address so the intial variable will not be changed and after completion of that function that variable is deleted
+// CLASS 3
+    // Whenever a expression is passed in pass by reference it gives error
+    int binarySearch(vector<int>& arr,int& s,int& e,int& key){
+        if(s>e)return -1;
+        int mid=(s+e)/2;
+        if(arr[mid]==key)return mid;
+        if(arr[mid]<key){
+            s=mid+1;
+____IMP_____            return binarySearch(arr,s,e,key);//binarySearch(arr,mid+1,e,key);//if you give expression(a+b) in pass by reference it gives error so we have used s=mid+1 to avoid this
+        }
+        else{
+            e=mid-1;
+            return binarySearch(arr,s,e,key);
+        }
