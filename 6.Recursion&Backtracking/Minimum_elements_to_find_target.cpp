6713574330a5d74745dbff8752da5b4dc4d582d4
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-03-21 21:46:46 +0530

    Create Minimum_elements_to_find_target.cpp

---
+++ 6.Recursion&Backtracking/Minimum_elements_to_find_target.cpp
@@ -0,0 +1,8 @@
+// A int array is given which contains number by using that number you have to find the targer by adding and give the minimum elements required to reach targer
+// eg:target:3 arr=[1,2]
+//   output=>2
+// Explaination:1+1+1=3 [1,1,1] 3 elements required to reach the target
+//              1+2=3   [1,2]   2 elements required to reach the target
+//              2+1=3   [2,1]  2 elements required to reach the target
+//             so the minimum elements required is two
+  
