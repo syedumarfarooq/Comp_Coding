4be44768cfb9d0e210ab2be1f20e5fd2b17c6343
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-03-21 23:19:09 +0530

    Create Cut_into_segments.cpp

---
+++ 6.Recursion&Backtracking/Cut_into_segments.cpp
@@ -0,0 +1,4 @@
+A rod is given of length N and length x,y,z are given the rod can be cut into the lengths of x,y,z and the rod has to be cut in such a way that it has maximum segments using x,y,z
+Example:N=7
+  x=5,y=2,z=2
+  output=>
