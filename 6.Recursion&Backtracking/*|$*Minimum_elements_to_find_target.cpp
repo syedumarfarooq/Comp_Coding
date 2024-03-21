31d640f715f49e7aa8bf098fde7571c73c9f92f9
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-03-21 22:27:55 +0530

    Update *|$*Minimum_elements_to_find_target.cpp

--- 6.Recursion&Backtracking/*|$*Minimum_elements_to_find_target.cpp
+++ 6.Recursion&Backtracking/*|$*Minimum_elements_to_find_target.cpp
@@ -36,3 +36,38 @@ int main()
 
     return 0;
 }
+//ANOTHER WAY
+// LOGIC TOLD BY LOVE BABBAR CODE BY UMAR
+// #include <iostream>
+// #include<vector>
+// #include<limits.h>
+// using namespace std;
+// int solve(vector<int>& arr,int target,int& output){
+//     if(target==output){
+//         return 0;
+//     }
+//     if(target<output){
+//         return INT_MAX;
+//     }
+//     int mini=INT_MAX;
+//     for(int i=0;i<arr.size();i++){
+//         int outputt=output+arr[i];
+//         int ans=solve(arr,target,outputt);
+//         if(ans!=INT_MAX){
+//             mini=min(mini,ans+1);
+//         }
+//     }
+//     return mini;
+// }
+// int main()
+// {
+//   vector<int> arr{1,2};
+//   int target=10;
+//   int output=0;
+//   int ans=solve(arr,target,output);
+//   cout<<"ans:"<<ans;
+//   return 0;
+
+//     return 0;
+// }
+
