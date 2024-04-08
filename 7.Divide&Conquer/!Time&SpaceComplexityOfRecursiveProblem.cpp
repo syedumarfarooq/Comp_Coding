1c5087e27667b61367ffba556f5fa6077709310a
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-04-08 15:41:28 +0530

    Update !Time&SpaceComplexityOfRecursiveProblem.cpp

--- 7.Divide&Conquer/!Time&SpaceComplexityOfRecursiveProblem.cpp
+++ 7.Divide&Conquer/!Time&SpaceComplexityOfRecursiveProblem.cpp
@@ -1,3 +1,10 @@
-FINDING TIME COMPLEXITY USING INTUITION
-TIME COMPLEXITY:(1 call ka time)*total no of calls
-1)
+// FINDING TIME COMPLEXITY USING INTUITION
+// TIME COMPLEXITY:(1 call ka time)*total no of calls
+// SPACE COMPLEXITY:total depth of the tree
+// 1)Counting
+void printArray(int all, int n){
+        if(n== 0) return;  //}
+        cout<<*a<<" ";     //}takes k time to process both combined 
+        printArray(a+1, n-1);
+      ｝
+time complexity:O(k*(n+1));
