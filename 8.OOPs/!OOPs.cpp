82270d8ad5d265359482a33f3ed3805b10bd3bed
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-04-26 19:04:45 +0530

    Update !OOPs.cpp

--- 8.OOPs/!OOPs.cpp
+++ 8.OOPs/!OOPs.cpp
@@ -1,3 +1,8 @@
 *//Class 1 ***************************************************************************************************************************************************************************
 OOPs
-It is a programming technique which revolves around object
+  It is a programming technique which revolves around object
+Object
+  1)state/properties
+  2)behaviour/method
+Class
+  using classes we can create user defined datatyes
