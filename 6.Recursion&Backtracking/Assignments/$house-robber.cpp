12c7dfa3aae5bb6e79a2f17e912b437738fd4a0d
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-04-29 08:04:13 +0530

    Update $house-robber.cpp

--- 6.Recursion&Backtracking/Assignments/$house-robber.cpp
+++ 6.Recursion&Backtracking/Assignments/$house-robber.cpp
@@ -27,7 +27,7 @@ public:
         }
         //solve for 1 case
         int robAmt1=nums[i]+robHelper(nums,i+2);
-        int robAmt2=0+robHelper(nums,i+2);
+        int robAmt2=0+robHelper(nums,i+1);
         return max(robAmt1,robAmt2);
     }
     int rob(vector<int>& nums) {
