e6271042c4cd45fdc504038870172094ffc4f0aa
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-07-27 03:07:37 +0530

    Update !OOPs.cpp

--- 8.OOPs/!OOPs.cpp
+++ 8.OOPs/!OOPs.cpp
@@ -295,7 +295,7 @@ Types of inheritance
 
 //Dynamic Memory Allocation
     // 2d array in heap memory
-    int **a=new int*[row];
+    int **arr=new int*[row];
     for(int i=0;i<row;i++){
         arr[i]=new int[col];
     }
