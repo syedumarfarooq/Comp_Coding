d54d14ece0ecfc30ad489fef8a31e5de90f65bdf
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-08-04 18:32:09 +0530

    Update !OOPs3.cpp

--- 8.OOPs/!OOPs3.cpp
+++ 8.OOPs/!OOPs3.cpp
@@ -1,4 +1,4 @@
-*//Abstraction************
+*//Abstraction************************************************************************
     // delivering only essential information to the outer world while masking the outer detail
     // it is design or programming method that separtes the interface from implementation
     *//Abstract class
@@ -47,3 +47,4 @@
             birddoesSomething(bird);
             return 0;
         }
+*//Inline function************************************************************************
