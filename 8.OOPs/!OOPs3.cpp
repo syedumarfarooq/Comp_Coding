46e7a3275964769c1b1502c06691d429447246ce
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-08-04 14:09:19 +0530

    Create !OOPs3.cpp

---
+++ 8.OOPs/!OOPs3.cpp
@@ -0,0 +1,3 @@
+*//Abstraction************
+    // delivering only essential information to the outer world while masking the outer detail
+    // it is design or programming method that separtes the interface from implementation
