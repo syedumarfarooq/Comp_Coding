2a4b93c52c2e57f5ec5d5484d6739e6116319cdd
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-07 18:33:24 +0530

    Update !Linked_Lists.cpp

--- 9.LinkedLists/!Linked_Lists.cpp
+++ 9.LinkedLists/!Linked_Lists.cpp
@@ -2,12 +2,46 @@
   *//Node creation*********
   // Singly Linked list
     class Node{
+      public:
       int data;
       Node *next;
-    }
+       Node() {
+                this->data = 0;
+                this->next = NULL;
+        }
+        Node(int data) {
+                this->data = data;
+                this->next = NULL;
+        }
+    } 
+ //Printing linked list
+    void print(Node* head) {
+   
+           Node* temp = head;
+           while(temp != NULL) {
+                   cout << temp->data << " ";
+                   temp = temp->next;
+           }
+   }
+   int main(){
+     Node* first=new Node(10);
+     Node* second=new Node(10);
+
+     Node* third=new Node(10);
+     Node* fourth=new Node(10);
+     Node* fifth=new Node(10);
+     
+     first->next=second;
+     second->next=third;
+     third->next=fourth;
+     fourth->next=fifth;
+     print(first);
+   }
+  
+  
     // Types of linked list
-    1.Singly ll
-    2.Circular ll
-    3.Doubly ll
-    4.Circular doubly ll
+     // 1.Singly ll
+     // 2.Circular ll
+     // 3.Doubly ll
+     // 4.Circular doubly ll
 
