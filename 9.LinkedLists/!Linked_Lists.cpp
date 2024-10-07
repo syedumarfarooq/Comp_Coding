2d55b0b65030b11f6ec16b4ce0c016a3772debbc
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-07 18:15:04 +0530

    Update !Linked_Lists.cpp

--- 9.LinkedLists/!Linked_Lists.cpp
+++ 9.LinkedLists/!Linked_Lists.cpp
@@ -1,1 +1,13 @@
- *//  Linked Lists***********
+*//  Linked Lists*******************
+  *//Node creation*********
+  // Singly Linked list
+    class Node{
+      int data;
+      Node *next;
+    }
+    // Types of linked list
+    1.Singly ll
+    2.Circular ll
+    3.Doubly ll
+    4.Circular doubly ll
+
