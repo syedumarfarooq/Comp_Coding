0849a807918a3e079c71348d46729909f26079c9
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-09 04:46:54 +0530

    Update !Linked_Lists.cpp

--- 9.LinkedLists/!Linked_Lists.cpp
+++ 9.LinkedLists/!Linked_Lists.cpp
@@ -219,7 +219,45 @@
               delete curr;
       
       }
-      
+      Node* reverse(Node* &prev, Node* &curr) {
+        //base case
+        if(curr == NULL) {
+                //LL reverse ho chuki
+                return prev;
+        }
+        
+        //1 case solve then recursion will take care
+        Node* forward = curr -> next;
+        curr -> next = prev;
+
+        reverse(curr, forward);
+}
+
+Node* reverseusingLoop(Node* head ) {
+        Node* prev = NULL;
+        Node* curr = head;
+
+        while(curr != NULL ) {
+                Node* temp = curr ->next;
+                curr ->next = prev;
+                prev = curr;
+                curr = temp;
+        }
+        return prev;
+}
+Node* reverseusingRecursion(Node* prev, Node* curr) {
+        //base case
+        if(curr == NULL) 
+                return prev;
+
+        Node* temp = curr ->next;
+        curr ->next = prev;
+        prev = curr;
+        curr = temp;
+
+        //recursion sambhal lega
+        return reverseusingRecursion(prev, curr);
+}
       int main() {
       
               Node* head = NULL;
