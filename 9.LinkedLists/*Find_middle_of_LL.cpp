0efea4608ac854956c803d33613065bdede9ece2
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-09 05:01:13 +0530

    Create *Find_middle_of_LL.cpp

---
+++ 9.LinkedLists/*Find_middle_of_LL.cpp
@@ -0,0 +1,4 @@
+//Finding the middle of the linked list if the list contains even then n/2 node is the middle and if the lenght is odd then n/2+1 is the middle
+//TWO POINTER APPROACH
+*//TORTOISE ALGORITHM*****
+*//SLOW AND FAST POINTER APPROACH******
