567339faeca4f14ecc3df9626cd0faaa9a96bd03
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-11 02:11:56 +0530

    Update *|$*Detect_or_delete_Loop.cpp

--- 9.LinkedLists/*|$*Detect_or_delete_Loop.cpp
+++ 9.LinkedLists/*|$*Detect_or_delete_Loop.cpp
@@ -44,7 +44,47 @@
       };
 *// -starting point of loop************
 Explained logic below
-      
+      /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+    ListNode *detectCycle(ListNode *head) {
+        if(head==NULL||head->next==NULL){
+            return NULL;
+            
+        }
+        ListNode* slow=head;
+        ListNode* fast=head;
+              while(fast!=NULL){
+                  fast=fast->next;
+                  if(fast!=NULL){
+                      fast=fast->next;
+                      slow=slow->next;
+                  }
+                  if(fast==slow){
+                    slow=head;
+                    break;
+                  }
+              }
+           if(fast==NULL){
+            return NULL;
+           }
+
+              while(slow!=fast){
+                slow=slow->next;
+                fast=fast->next;
+              }
+              return slow;
+
+    }
+};
 eg:1-2-3-4-5-6
          |    |
          9-8-7
