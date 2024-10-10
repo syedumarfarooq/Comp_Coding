4356b1aa5b1527d7991109103cbe0511e747617d
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-10 23:37:43 +0530

    Update *Find_middle_of_LL.cpp

--- 9.LinkedLists/*Find_middle_of_LL.cpp
+++ 9.LinkedLists/*Find_middle_of_LL.cpp
@@ -80,3 +80,32 @@ public:
         return slow;
     }
 };
+//my method
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(slow!=NULL&&fast!=NULL){
+            fast=fast->next;
+            if(fast!=NULL){
+                fast=fast->next;
+              if(fast!=NULL){
+                slow=slow->next;
+              }
+                
+            }
+        }
+        return slow;
+    }
+};
