0a9a89af83248e39cc958bc1745b3f84d8e9bbb4
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-11 00:59:24 +0530

    Update *|$*Detect_or_delete_Loop.cpp

--- 9.LinkedLists/*|$*Detect_or_delete_Loop.cpp
+++ 9.LinkedLists/*|$*Detect_or_delete_Loop.cpp
@@ -1,5 +1,46 @@
-// In this the question may be asked 
-// -check if there is a loop present
+// In detect or delete loop, the question may be asked 
+*// -check if there is a loop present**********
       // Floyd cycle detection algorithm i.e slow fast pointer
-// -starting point of loop
-// -remove loop
+      https://leetcode.com/problems/linked-list-cycle/description/
+      // Given head, the head of a linked list, determine if the linked list has a cycle in it.
+      
+      // There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+      
+      // Return true if there is a cycle in the linked list. Otherwise, return false.
+      
+       
+      
+      // Example 1:
+      
+      
+      // Input: head = [3,2,0,-4], pos = 1
+      // Output: true
+      // Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+      /**
+       * Definition for singly-linked list.
+       * struct ListNode {
+       *     int val;
+       *     ListNode *next;
+       *     ListNode(int x) : val(x), next(NULL) {}
+       * };
+       */
+      class Solution {
+      public:
+          bool hasCycle(ListNode *head) {
+              ListNode* slow=head;
+              ListNode* fast=head;
+              while(fast!=NULL){
+                  fast=fast->next;
+                  if(fast!=NULL){
+                      fast=fast->next;
+                      slow=slow->next;
+                  }
+                  if(fast==slow){
+                      return true;
+                  }
+              }
+              return false;
+          }
+      };
+*// -starting point of loop************
+*// -remove loop****************
