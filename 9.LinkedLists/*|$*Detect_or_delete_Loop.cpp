de2b3e4a2f1eb0c51024550eaa412c42c61028ad
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-11 00:54:51 +0530

    Update and rename Detect_or_delete_Loop.cpp to *|$*Detect_or_delete_Loop.cpp

--- 9.LinkedLists/*|$*Detect_or_delete_Loop.cpp
+++ 9.LinkedLists/*|$*Detect_or_delete_Loop.cpp
@@ -1,5 +1,5 @@
 // In this the question may be asked 
 // -check if there is a loop present
-      // Floyd cycle detection algorithm
+      // Floyd cycle detection algorithm i.e slow fast pointer
 // -starting point of loop
 // -remove loop
