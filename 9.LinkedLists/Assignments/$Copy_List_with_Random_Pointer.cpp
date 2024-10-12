d54b087ca6131f954a5358713f77b3283d4a220d
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-13 03:27:38 +0530

    Update $Copy_List_with_Random_Pointer.cpp

--- 9.LinkedLists/Assignments/$Copy_List_with_Random_Pointer.cpp
+++ 9.LinkedLists/Assignments/$Copy_List_with_Random_Pointer.cpp
@@ -20,23 +20,71 @@ public:
 */
 *// Time Complexity is O(n)*******
 
-class Solution {
-public:
-    Node* helper(Node* head,unordered_map<Node*,Node*>&mp){
-        if(head==NULL)return NULL;
-        Node* newHead=new Node(head->val);
-        mp[head]=newHead;
-        newHead->next=helper(head->next,mp);
-        if(head->random){
-            newHead->random=mp[head->random];
-        }
-        return newHead;
-    }
+*// class Solution {
+// public:
+//     Node* helper(Node* head,unordered_map<Node*,Node*>&mp){
+//         if(head==NULL)return NULL;
+//         Node* newHead=new Node(head->val);
+//         mp[head]=newHead;
+//         newHead->next=helper(head->next,mp);
+//         if(head->random){
+//             newHead->random=mp[head->random];
+//         }
+//         return newHead;
+//     }
 
-    Node* copyRandomList(Node* head) {
+//     Node* copyRandomList(Node* head) {
         
-        unordered_map<Node*,Node*>mp;
-        return helper(head,mp); 
+//         unordered_map<Node*,Node*>mp;
+//         return helper(head,mp); 
+//     }
+// };****
+*// Time Complexity is O(1)*******
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
     }
 };
-*// Time Complexity is O(1)*******
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(head==NULL)return NULL;
+        Node* it=head;
+        while(it!=NULL){
+            Node* newNode=new Node(it->val);
+            newNode->next=it->next;
+            it->next=newNode;
+            it=it->next->next;
+        }
+        it=head;
+        while(it){
+            Node* cloneNode=it->next;
+            cloneNode->random=it->random?it->random->next:NULL;
+            it=it->next->next;
+
+        }
+        
+        it=head;
+        Node* cloneHead=it->next;
+        while(it){
+            Node* cloneNode=it->next;
+            it->next=it->next->next;
+            if(cloneNode->next){
+                cloneNode->next=cloneNode->next->next;
+            }
+            it=it->next;
+        }
+        return cloneHead;
+    }
+};
