a4520333aeabb5570d179393bc7118231f27b33e
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-13 20:20:49 +0530

    Update !Stacks.cpp

--- 8.OOPs/10.Stacks/!Stacks.cpp
+++ 8.OOPs/10.Stacks/!Stacks.cpp
@@ -1,1 +1,19 @@
-h
+*// Stack****
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+  //creation
+  stack<int> st;
+  //insertion
+  st.push(10);
+  st.push(20);
+  //remove
+  st.pop();
+  cout<<st.top();
+  cout<<st.size();
+  if(st.empty()){
+    cout<<"empty";
+  }
+  
+}
