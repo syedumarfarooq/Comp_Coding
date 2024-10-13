7486d4207854f53c778cc521a609d77ee342c231
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-13 23:21:38 +0530

    Update Insert_At_Bottom.cpp

--- 10.Stacks/Insert_At_Bottom.cpp
+++ 10.Stacks/Insert_At_Bottom.cpp
@@ -1,1 +1,21 @@
-Insert the top element of the stack at the bottom of the same stack
+// Insert the top element of the stack at the bottom of the same stack
+void solve(string<int> &s,int target){
+  if(s.empty()){
+    s.push(target);
+    return;
+  }
+  int temp=s.top();
+  s.pop();
+  solve(s,target);
+  s.push(temp);
+}
+void insertAtBottom(string<int> &s){
+  if(s.empty()){
+    cout<<"stack empty";
+    return;
+  }
+  int target=s.top();
+  s.pop();
+  solve(s,target);
+  
+}
