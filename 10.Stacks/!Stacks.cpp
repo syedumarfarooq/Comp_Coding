0bd1df0e29852c8e32db810c5cc6bfd70ccb6d5b
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-13 23:01:10 +0530

    Update !Stacks.cpp

--- 10.Stacks/!Stacks.cpp
+++ 10.Stacks/!Stacks.cpp
@@ -176,16 +176,59 @@
           return 0;
         }
 *//Reversing string using stack*******
-int main(){
-    string="umar";
-    stack<char> s;
-    for(int i=0;i<string.length();i++){
-        s.push(string[i]);
-    }
-    while(!s.empty()){
-        cout<<s.top();
-        s.pop();
-    }
-    cout<<endl;
-    return 0;
-}
+        int main(){
+            string="umar";
+            stack<char> s;
+            for(int i=0;i<string.length();i++){
+                s.push(string[i]);
+            }
+            while(!s.empty()){
+                cout<<s.top();
+                s.pop();
+            }
+            cout<<endl;
+            return 0;
+        }
+*//Finding middle Element********
+        #include <iostream>
+        #include<stack>
+        using namespace std;
+        
+        void printMiddle(stack<int> &s, int &totalSize) {
+                if(s.size() == 0 ) {
+                        cout << "There is no element in stack" << endl;
+                        return ;
+                }
+                //base case
+                if(s.size() == totalSize/2 + 1) {
+                        cout << "Middle element is: " << s.top() << endl;
+                        return;
+                }
+        
+                int temp = s.top();
+                s.pop();
+        
+                //recursive call
+                printMiddle(s, totalSize);
+        
+                //backtrack;
+                s.push(temp);
+        
+        }
+        
+        int main() {
+                stack<int> s;
+        
+                s.push(10);
+                s.push(20);
+                s.push(30);
+                s.push(40);
+                s.push(50);
+                s.push(60);
+                s.push(70);
+        
+                int totalSize = s.size();
+                printMiddle(s,totalSize);
+        
+                return 0;
+        }
