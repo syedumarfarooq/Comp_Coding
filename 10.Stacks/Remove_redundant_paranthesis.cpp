382a57dc171305e9c5582db998f1f63fc791f50c
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-14 01:19:20 +0530

    Delete 10.Stacks/Remove_redundant_paranthesis.cpp

--- 10.Stacks/Remove_redundant_paranthesis.cpp
+++
@@ -1,7 +0,0 @@
-eg:
-(a+b)=>no redundant parenthesis
-(a+b+c)*(d))=>yes 
-(a*b+(c/d))=>no
-((a/b))=>yes
-  LOGIC:
-if there is any operator(+-*/) present inbetween the brackets then it is not redundant
