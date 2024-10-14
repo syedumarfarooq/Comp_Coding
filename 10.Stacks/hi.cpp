3577590ddc14dcbb7de96c285f30c1c9f322550c
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-14 06:15:43 +0530

    Create hi.cpp

---
+++ 10.Stacks/hi.cpp
@@ -0,0 +1,24 @@
+// Finding the next smallest element
+// eg:2,1,4,3
+//   =>1,-1,3,-1
+//   after 2 the next smallest element is 1
+//   after 1 there is no  smallest element so -1 
+//   after 4 the next smallest element is 3
+//   after 3 there is no  smallest element so -1 
+
+
+
+vector<int> nextSmallest(vector<int> v){
+  stack<int> s;
+  s.push(-1);
+  vector<int> ans(v.size());
+  for(int i=v.size()-1;i>=0;i--){
+      int curr=v[i];
+      while(s.top()>=curr){
+        s.pop();
+      }
+      ans[i]=s.top();
+      s.push(curr);
+  }
+  return ans;
+}
