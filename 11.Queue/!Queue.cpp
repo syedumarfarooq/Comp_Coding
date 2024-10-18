d66e4b7bb23fb83a46ead8e30a78172a6609f6a4
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-18 22:38:36 +0530

    Update !Queue.cpp

--- 11.Queue/!Queue.cpp
+++ 11.Queue/!Queue.cpp
@@ -1,1 +1,19 @@
-h
+*// Queue******
+#include<queue>
+int main(){
+  queue<int> q;
+  q.push(10);
+  q.push(20);
+  cout<<q.size();
+  q.pop();//=>10
+  if(q.empty()){
+    cout<<"queue is empty";
+  }else{
+    cout<<"queue not empty":
+  }
+  cout<<"queue front element"<<q.front();
+}
+*//Queue without stl******
+class Queue{
+  
+};
