a94c6836b3800d4cf0c23b596a907f5da96e5d2a
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-19 06:47:02 +0530

    Update Reverse_Queue.cpp

--- 11.Queue/Reverse_Queue.cpp
+++ 11.Queue/Reverse_Queue.cpp
@@ -12,3 +12,13 @@ void reverseQueue(queue<int> &q){
     q.push(element);
   }
 }
+*//Using recursion*****
+void reverseQueue(queue<int> &q){
+  if(q.empty()){
+    return;
+  }
+  int element=q.front();
+  q.pop();
+  reverseQueue(q);
+  q.push(element);
+}
