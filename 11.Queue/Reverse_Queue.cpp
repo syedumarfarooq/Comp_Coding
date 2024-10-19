69d3f981c9f446602d553ba76c09b8859ca014e6
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-19 06:41:31 +0530

    Create Reverse_Queue.cpp

---
+++ 11.Queue/Reverse_Queue.cpp
@@ -0,0 +1,14 @@
+*//Using stack*****
+void reverseQueue(queue<int> &q){
+  stack<int> s;
+  while(!q.empty()){
+    int element=q.front();
+    q.pop();
+    s.push(element);
+  }
+  while(!s.empty()){
+    int element=s.top();
+    s.pop();
+    q.push(element);
+  }
+}
