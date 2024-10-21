ccb67a90234f9d313a0e5b75231c7e7e663d0c0a
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-21 09:22:19 +0530

    Update !Trees.cpp

--- 12.Trees/!Trees.cpp
+++ 12.Trees/!Trees.cpp
@@ -2,3 +2,4 @@
 Non-liner type of data structure
 *//Binary Tree******
 A trees which has only atmost 2 children nodes 
+-If a tree only has left children or only right children then they are call *//Skew tree**
