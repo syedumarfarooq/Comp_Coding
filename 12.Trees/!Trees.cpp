162cf4e1faabe4aeedf81c3b55639cb2f4fb08bd
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-22 23:31:47 +0530

    Update !Trees.cpp

--- 12.Trees/!Trees.cpp
+++ 12.Trees/!Trees.cpp
@@ -44,6 +44,59 @@ void levelOrderTraversal(Node* root){
     }
   }
 }
+void levelOrderTraversalprintinginlevel(Node* root){//printing the node in level wise eg:1
+                                                                        //2 3
+  queue<Node*> q;
+  q.push(root);
+  q.push(NULL);//after first root node inserting null then whenever null comes will push null 
+  //after this it will iterate all its children and all to the queue after all its childre it will come on NULL when NULL comes we are adding 
+  //NULL again telling the end of all the children of current level
+  while(!q.empty()){
+    Node* temp=q.front();
+    q.pop();
+    if(temp==NULL){//
+      cout<<endl;
+      if(!q.empty()){
+        q.push(NULL);
+      }
+    }else{
+      cout<<temp->data<<" ";
+    if(temp->left){
+      q.push(temp->left);
+      
+    }
+    if(temp->right){
+      q.push(temp->right);
+      
+    }
+    }
+    
+  }
+}
+void inOrderTraversal(Node* root){
+  if(root==NULL){
+    return ;
+  }
+  inOrderTraversal(root->left);
+  cout<<root->data;
+  inOrderTraversal(root->right);
+}
+void preOrderTraversal(Node* root){
+   if(root==NULL){
+    return ;
+  }
+  cout<<root->data;
+  preOrderTraversal(root->left);
+  preOrderTraversal(root->right);
+}
+void postOrderTraversal(Node* root){
+  if(root==NULL){
+    return;
+  }
+  postOrderTraversal(root->left);
+  postOrderTraversal(root->right);
+  cout<<root->data;
+}
 int main(){
   Node* root=NULL;
   root=buildTree();
