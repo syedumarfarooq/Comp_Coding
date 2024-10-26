964037f14007310eb16d5b1c1ca58196bc6010c1
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-26 22:46:21 +0530

    Update Fast_way_to_find_diameter.cpp

--- 12.Trees/Assignments/Fast_way_to_find_diameter.cpp
+++ 12.Trees/Assignments/Fast_way_to_find_diameter.cpp
@@ -1,4 +1,5 @@
 https://leetcode.com/problems/diameter-of-binary-tree/description/
+https://github.com/syedumarfarooq/Comp_Coding/blob/main/12.Trees/*Diameter_of_BT.cpp
 // Given the root of a binary tree, return the length of the diameter of the tree.
 
 // The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
