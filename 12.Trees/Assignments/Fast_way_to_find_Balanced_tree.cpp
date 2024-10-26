7b3a77abe825d9b20398440d4bf593c3e6c14847
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-26 22:45:31 +0530

    Create Fast_way_to_find_Balanced_tree.cpp

---
+++ 12.Trees/Assignments/Fast_way_to_find_Balanced_tree.cpp
@@ -0,0 +1,42 @@
+https://leetcode.com/problems/balanced-binary-tree/description/
+// Given a binary tree, determine if it is 
+// height-balanced
+// .
+
+ 
+
+// Example 1:
+
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: true
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isbalanced=true;
+    int height(TreeNode* root){
+        if(root==NULL){
+            return 0;
+        }
+        int lh=height(root->left);
+        int rh=height(root->right);
+        if(isbalanced && abs(lh-rh)>1){
+            isbalanced=false;
+        }
+        return max(lh,rh)+1;
+    }
+    bool isBalanced(TreeNode* root) {
+        height(root);
+        return isbalanced;
+    }
+};
