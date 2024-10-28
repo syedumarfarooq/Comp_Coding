7d006995a9773fcbd95fa74d56b7e9e850572b6f
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-29 01:30:12 +0530

    Create Flatten_BST_to_LL.cpp

---
+++ 13.Binary Search Tree/Assignments/Flatten_BST_to_LL.cpp
@@ -0,0 +1,17 @@
+void in(Node* root,Node* &prev){
+  if(!root)return;
+  in(root->left,prev);
+  prev->left=0;
+  prev->right=root;
+  prev=root;
+  in(root->right,prev);
+}
+Node* flatten(Node* root){
+  Node* dummy=new Node(-1);
+  Node* prev=dummy;
+  in(root,prev);
+  prev->left=0;//as the last node of the tree much be pointed to null
+  prev->right=0;
+  root=dummy->right;//the answer starts from dummy's right;
+  return root;
+}
