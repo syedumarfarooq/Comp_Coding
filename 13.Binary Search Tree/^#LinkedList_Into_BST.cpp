65b568340ed785455b099642c4c427fd69fc7058
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-28 14:35:25 +0530

    Update ^#LinkedList_Into_BST.cpp

--- 13.Binary Search Tree/^#LinkedList_Into_BST.cpp
+++ 13.Binary Search Tree/^#LinkedList_Into_BST.cpp
@@ -2,10 +2,10 @@ Node* sortedLinkedListIntoBst(Node* &head,int n){//head is the starting point of
   if(n<=0||head==NULL){
     return NULL:
   }
-  Node* leftSubTree=sortedLinkedListIntoBst(head,n-1-n/2);//left node is n-main node(1)-rest of the half;
+  Node* leftSubTree=sortedLinkedListIntoBst(head,n/2);
   Node* root=head;
   root->left=leftSubTree;
   head=head->right;//students forget this line
-  root->right=sortedLinkedListIntoBst(head,n/2);
+  root->right=sortedLinkedListIntoBst(head,n-1-n/2);//left node is n-main node(1)-rest of the half;
   return root;
 }
