411ae38cb5d393a8d077f6eb30a38786b9633f25
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-28 14:29:16 +0530

    Create ^#LinkedList_Into_BST.cpp

---
+++ 13.Binary Search Tree/^#LinkedList_Into_BST.cpp
@@ -0,0 +1,11 @@
+Node* sortedLinkedListIntoBst(Node* &head,int n){//head is the starting point of linked list
+  if(n<=0||head==NULL){
+    return NULL:
+  }
+  Node* leftSubTree=sortedLinkedListIntoBst(head,n-1-n/2);//left node is n-main node(1)-rest of the half;
+  Node* root=head;
+  root->left=leftSubTree;
+  head=head->right;
+  root->right=sortedLinkedListIntoBst(head,n/2);
+  return root;
+}
