6899f952cd8d28df224f664ecf0cadf979bb358c
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-28 15:34:46 +0530

    Create Largest_BST_in_a_Binary_Tree.cpp

---
+++ 13.Binary Search Tree/Largest_BST_in_a_Binary_Tree.cpp
@@ -0,0 +1,48 @@
+https://www.geeksforgeeks.org/largest-bst-binary-tree-set-2/
+class NodeData {
+	public:
+	int size;
+	int minVal;
+	int maxVal;
+	bool validBST;
+
+	NodeData() {
+		
+	}
+	NodeData(int size, int max, int min, bool valid) {
+		this->size = size;
+		minVal = min;
+		maxVal = max;
+		validBST = valid;
+	}
+
+};
+NodeData* findLargestBST(Node* root, int &ans) {
+	//base case
+	if(root == NULL) {
+		NodeData* temp  = new NodeData(0, INT_MIN, INT_MAX, true);
+		return temp;
+	}
+
+	NodeData* leftKaAns = findLargestBST(root->left, ans);
+	NodeData* rightKaAns = findLargestBST(root->right,ans);
+
+	//checking starts here
+	NodeData* currNodeKaAns = new NodeData();
+
+	currNodeKaAns->size = leftKaAns->size + rightKaAns->size + 1;
+	currNodeKaAns->maxVal = max(root->data, rightKaAns->maxVal);
+	currNodeKaAns->minVal = min(root->data, leftKaAns->minVal);
+
+	if(leftKaAns->validBST && rightKaAns->validBST && (root->data > leftKaAns->maxVal && root->data < rightKaAns->minVal) ) {
+		currNodeKaAns->validBST = true;
+	}
+	else {
+		currNodeKaAns->validBST = false;
+	}
+
+	if(currNodeKaAns->validBST) {
+		ans = max(ans, currNodeKaAns->size);
+	}
+	return currNodeKaAns;
+}
