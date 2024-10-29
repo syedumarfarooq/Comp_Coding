4bd331925850dff62d7c608d1747441f187a93f9
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-30 02:18:07 +0530

    Update !Heaps.cpp

--- 14.Heaps/!Heaps.cpp
+++ 14.Heaps/!Heaps.cpp
@@ -1,1 +1,4 @@
 *//Heaps***
+// It is a complete binary tree that satifies the heap property
+// A complete binary tree is a binary tree in which every level, except possibly the last, is completely filled, and all nodes in the last 
+// level are as far left as possible.
