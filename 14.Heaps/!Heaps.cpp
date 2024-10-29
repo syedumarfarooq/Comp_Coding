f2759e0bb5072db66bf25b64bc05eafccb0b821f
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-30 03:52:28 +0530

    Update !Heaps.cpp

--- 14.Heaps/!Heaps.cpp
+++ 14.Heaps/!Heaps.cpp
@@ -70,13 +70,7 @@ class Heap{
 
 int main() {
   Heap h;
-//   h.arr[0] = -1;
-//   h.arr[1] = 100;
-//   h.arr[2] = 50;
-//   h.arr[3] = 60;
-//   h.arr[4] = 40;
-//   h.arr[5] = 45;
-//   h.size = 5;
+
    h.insert(50);
     h.insert(30);
      h.insert(70);
@@ -89,14 +83,7 @@ int main() {
           cout << h.arr[i] << " ";
   }cout << endl;
 
-//   h.insert(110);
-//   cout << endl << "printing the heap " << endl;
-//   for(int i=0; i<=h.size; i++) {
-//           cout << h.arr[i] << " ";
-//   }cout << endl;
-
-
-
-
   return 0;
 }
+*//Heapify****
+//convert any array into heap we use heapify function
