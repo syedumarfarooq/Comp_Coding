96e096277d581f2e6bf24bda2e883a7e78d50831
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-31 02:50:07 +0530

    Update !Heaps2.cpp

--- 14.Heaps/!Heaps2.cpp
+++ 14.Heaps/!Heaps2.cpp
@@ -66,3 +66,15 @@ int main() {
 *//Tip**
 // Generally kth smallest, kth greatest, merge k sorted array and merge ke sorted LL (where ever k types questions)generally these questions
 // can be solved using in heap but not ALL no logical reason just based on expierence
+*//HW**
+// 1.convert a bst(it is already complete) into max heap 
+        //inorder of bst will give u a ascending ordered list using this place them using postorder eg:1,2,3 first place 1 on left 2 on right
+        //then three as main node
+//2.check if the tree is complete
+        //first by level travesal add pair into the queue <count,node*> then whenever there is no child add null if the parent is null then
+        //stop this must have counted all the node + null spaces ,then if the count>total count of the nodes then it is not bst
+        // Or
+        //RECURSIVEly
+        //if only left child true if right child false and if leaf node then true and has both child true
+        //using my thinking adding some points if ther is one left only child in that same level there should not be any node after that
+        //for this we might have to consider level count and distance count
