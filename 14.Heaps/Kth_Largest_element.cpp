a4b7fefe244387306063b4bff3e5a8734a652666
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-30 19:23:39 +0530

    Create Kth_Largest_element.cpp

---
+++ 14.Heaps/Kth_Largest_element.cpp
@@ -0,0 +1,36 @@
+https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+// Given an integer array nums and an integer k, return the kth largest element in the array.
+
+// Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+// Can you solve it without sorting?
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,2,1,5,6,4], k = 2
+// Output: 5
+// Example 2:
+
+// Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+// Output: 4
+*//Logic*
+  // min heap because it contains the kth largest element at the top of k elements in a heap
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        //min heap
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int i=0;i<k;i++){
+            pq.push(nums[i]);
+        }
+        for(int i=k;i<nums.size();i++){
+            if(nums[i]>pq.top()){
+                pq.pop();
+                pq.push(nums[i]);
+            }
+        }
+        return pq.top();
+    }
+};
