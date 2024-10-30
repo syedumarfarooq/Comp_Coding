e4214642337e6a85371b937e5c1f4133ff729d5a
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-31 01:59:45 +0530

    Update Largest_BST_in_a_Binary_Tree.cpp

--- 13.Binary Search Tree/Largest_BST_in_a_Binary_Tree.cpp
+++ 13.Binary Search Tree/Largest_BST_in_a_Binary_Tree.cpp
@@ -31,7 +31,11 @@ NodeData* findLargestBST(Node* root, int &ans) {
 	NodeData* currNodeKaAns = new NodeData();
 
 	currNodeKaAns->size = leftKaAns->size + rightKaAns->size + 1;
-	currNodeKaAns->maxVal = max(root->data, rightKaAns->maxVal);
+	currNodeKaAns->maxVal = max(root->data, rightKaAns->maxVal);//max value is found in the right part and it stores this bcz we can 
+	//compare the next root node if it lies within the range
+	//with the max and min values of a tree we can check if the root is a bst for eg:50 is the root node right child is 60 and its left
+	//child is 45 if we would have compared 50 diretcly with 60 then we would have considered bst so we compare the minimum from the 
+	//right side and it should be greater than the root node if not then it is not bst
 	currNodeKaAns->minVal = min(root->data, leftKaAns->minVal);
 
 	if(leftKaAns->validBST && rightKaAns->validBST && (root->data > leftKaAns->maxVal && root->data < rightKaAns->minVal) ) {
