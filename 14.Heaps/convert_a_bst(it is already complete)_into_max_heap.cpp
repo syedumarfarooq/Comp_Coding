8f718c6dfdd013ffecbeef40507582efd63d846d
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-31 02:54:18 +0530

    Update convert_a_bst(it is already complete)_into_max_heap.cpp

--- 14.Heaps/convert_a_bst(it is already complete)_into_max_heap.cpp
+++ 14.Heaps/convert_a_bst(it is already complete)_into_max_heap.cpp
@@ -1,2 +1,3 @@
+*//HOMEWORK***
 //inorder of bst will give u a ascending ordered list using this place them using postorder eg:1,2,3 first place 1 on left 2 on right
         //then three as main node
