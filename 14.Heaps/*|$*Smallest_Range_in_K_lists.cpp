8d5356401ebdc9e3b1b80895fc056af96a9a01cd
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-31 23:27:40 +0530

    Update *|$*Smallest_Range_in_K_lists.cpp

--- 14.Heaps/*|$*Smallest_Range_in_K_lists.cpp
+++ 14.Heaps/*|$*Smallest_Range_in_K_lists.cpp
@@ -3,4 +3,4 @@
 //in the end we have to get smallest range for making the minimum and maximum range smaller we have to either increase the minimum or decrease
 //the maximum but as the question say non descending order if we go to next element(as linked list cannot go back)
 //it increase and there is no chance of decreases or going back i.e  we are increasing the minimum that is why we are using min heap so that
-// we can delete the minimum number and go further i.e next element to create range
+// we can delete the minimum number and go further i.e next element to create range by adding the element from the same list u deleted earlier
