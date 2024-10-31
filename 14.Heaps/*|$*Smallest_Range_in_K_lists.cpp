c545c9d136514b11a7c1809e94709e5cf983a1aa
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-10-31 23:29:13 +0530

    Update *|$*Smallest_Range_in_K_lists.cpp

--- 14.Heaps/*|$*Smallest_Range_in_K_lists.cpp
+++ 14.Heaps/*|$*Smallest_Range_in_K_lists.cpp
@@ -1,5 +1,5 @@
 // if we take all elements a from from every k list and make a min and maximum range then all the number  of every row will lie
-// in that range
+// in that range so we have to find that range and the minimum difference is the output
 //in the end we have to get smallest range for making the minimum and maximum range smaller we have to either increase the minimum or decrease
 //the maximum but as the question say non descending order if we go to next element(as linked list cannot go back)
 //it increase and there is no chance of decreases or going back i.e  we are increasing the minimum that is why we are using min heap so that
