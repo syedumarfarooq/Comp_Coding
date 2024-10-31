2c4e3883edfda7f2b83cafd3e84d6d52fb071ea7
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-11-01 01:42:02 +0530

    Create Remove_Stones_to_Minimize_the_Total.cpp

---
+++ 14.Heaps/Remove_Stones_to_Minimize_the_Total.cpp
@@ -0,0 +1,2 @@
+//as the question say we can divide a stone by 2 and subtract from the total and this operation can be done k times on any of the stone to minimize the total 
+//Basic logic say if we do the operation on the bigger rocks than only we can minimize the total 
