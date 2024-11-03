341d32c2a2e19d57f6bd63a3c8153f62d8456baa
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-11-03 06:12:54 +0530

    Create !Tries.cpp

---
+++ 15.Hashmaps&Tries/!Tries.cpp
@@ -0,0 +1,3 @@
+*//Tries
+// It is a data structure which excist in a form  multi way tree(multiple children) it is mostly used in pattern search
+// This pattern search thing using tries will give time complexity directly propotional to the pattern length so it is used
