de39c80c3b0680e92da1ac73b57143d5b7758cb8
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-11-03 06:19:05 +0530

    Update !Tries.cpp

--- 15.Hashmaps&Tries/!Tries.cpp
+++ 15.Hashmaps&Tries/!Tries.cpp
@@ -1,3 +1,19 @@
 *//Tries
 // It is a data structure which excist in a form  multi way tree(multiple children) it is mostly used in pattern search
 // This pattern search thing using tries will give time complexity directly propotional to the pattern length so it is used
+Eg;umar,syed,love,lovely,car,care,caring
+        //       Root 
+        //   /  /  \   \
+        // u   s   l    c
+        // |   |    |    |
+        // m   y   o    a
+        // |   |    |    |
+        // a   e    v    r
+        // |   |    |    | \
+        // r   d    e    e  i
+        //          |       |
+        //          l       n
+        //          |       |
+        //          y       g
+  this is how multi way tree looks the root will create child if there staring index is different
+  
