f3069421178c3fb0f69e5a238274a0fcc6bdf43d
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-11-03 06:07:15 +0530

    Update !Hashmaps.cpp

--- 15.Hashmaps&Tries/!Hashmaps.cpp
+++ 15.Hashmaps&Tries/!Hashmaps.cpp
@@ -77,8 +77,9 @@ int main(){
 // 2)assume there are multiple values trying to be inserted at the same place then it creates linked list so atleast then the time complexity is O(N) as trying to access
 // the linked list but we have created the hash function so strong that load factor is <0.7 i.e(n/b) this wont let it happen i.e it wont let many values pointing to the same index
 // as it is alloted to free spaces so this case wont excist 
+  *//Question****
 // Check Linked List is a circular ll or has a loop present in it
-*#//Logic
+*//Logic
   // we keep on adding node to the map so whenever there is a node which is already added then it is a circular
 
 bool checkCircular(Node* head){
