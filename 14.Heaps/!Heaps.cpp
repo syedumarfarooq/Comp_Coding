98b1c8327f5e20b79d654c1b02d0059e909211e8
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-11-06 01:07:54 +0530

    Update !Heaps.cpp

--- 14.Heaps/!Heaps.cpp
+++ 14.Heaps/!Heaps.cpp
@@ -2,6 +2,7 @@
 // It is a complete binary tree that satifies the heap property
 // A complete binary tree is a binary tree in which every level, except possibly the last, is completely filled, and all nodes in the last 
 // level are as far left as possible.
+//heap property - either children show be less than root(max heap) or greater than root(min heap)
 #include <iostream>
 using namespace std;
 
