099211a3933bc9f49c8a8aa223f752ed54dde906
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-11-07 18:14:55 +0530

    Update Coin_Change.cpp

--- 16.Dynamic_Programming/Coin_Change.cpp
+++ 16.Dynamic_Programming/Coin_Change.cpp
@@ -48,3 +48,66 @@ https://leetcode.com/problems/coin-change/description/
 //         }
 //     }
 // };
+*//Top down method**
+//  class Solution {
+// public:
+//     int solveMem(vector<int>& coins,int amount,vector<int>&dp){
+//         if(amount==0){
+//             return 0;
+//         }
+//         if(amount<0){
+//             return INT_MAX;//we are not considering the not possible solution
+//         }
+//         if(dp[amount]!=-1){
+//             return dp[amount];
+//         }
+//         int mini=INT_MAX;
+//         for(int i=0;i<coins.size();i++){
+//             int ans=solveMem(coins,amount-coins[i],dp);
+//             if(ans!=INT_MAX){
+//                 mini=min(mini,1+ans);
+//             }
+//         }
+//         dp[amount]=mini;
+//         return dp[amount];
+//     }
+//     int coinChange(vector<int>& coins, int amount) {
+//         vector<int> dp(amount+1,-1);
+//         int ans=solveMem(coins,amount,dp);
+//         if(ans==INT_MAX){
+//             return -1;
+//         }else{
+//             return ans;
+//         }
+//     }
+// };
+ *//Bottom up or tabular method
+ class Solution {
+public:
+    int solveTab(vector<int>& coins,int amount){
+        vector<int> dp(amount+1,INT_MAX);
+        dp[0]=0;
+
+        
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<coins.size();j++){
+                if(i-coins[j]>=0&&dp[i-coins[j]]!=INT_MAX){
+                    int ans=dp[i-coins[j]];
+                    dp[i]=min(dp[i],ans+1);
+                }
+                
+            }
+        }
+        
+        return dp[amount];
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        
+        int ans=solveTab(coins,amount);
+        if(ans==INT_MAX){
+            return -1;
+        }else{
+            return ans;
+        }
+    }
+};
