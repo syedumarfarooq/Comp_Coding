26650a0f06d8fce42b3e64228be8dc1d799c2f85
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-11-09 01:43:50 +0530

    Update ^0-1_Knapsack_Problem.cpp

--- 16.Dynamic_Programming/^0-1_Knapsack_Problem.cpp
+++ 16.Dynamic_Programming/^0-1_Knapsack_Problem.cpp
@@ -77,3 +77,94 @@ https://www.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1?itm_source=gee
 //         return ans;
 //     }
 // };
+*//Using Tabular method
+//  class Solution {
+//   public:
+//     // Function to return max value that can be put in knapsack of capacity.
+    
+
+//     int solveUsingTabular(int capacity,int index,vector<int> &val, vector<int> &wt){
+//         //index= index of last element , and we are traversing right to left
+        
+//         //here 2 d array tell for that index and capacity(j) what is the maximum value can be put
+//         // eg:capacity = 4, val[] = [1, 2, 3], wt[] = [4, 5, 1]  here for index 0 the value
+//         // 1 value can be placed if capacity is 4 or more as it takes that much of capacity
+//         int n=val.size()-1;
+//         vector<vector<int>>dp(n+1,vector<int>(capacity+1,0));
+        
+//         // at index i the value i will be placed only if it has that much weight left in the bag 
+//         //so the capacity start from weight[0] as the value of i can be placed if weight i is left
+//         for(int w=wt[0];w<=capacity;w++){
+//             if(wt[0]<=capacity){
+//                 dp[0][w]=val[0];
+//             }else{
+//                 dp[0][w]=0;
+//             }
+//         }
+        
+//         for(int index=1; index<=n; index++) {
+//     for(int w=0; w<=capacity; w++) {
+//       //include and exclude
+//       int include = 0;
+//       if(wt[index] <= w)
+//         include = val[index] + dp[index-1][w - wt[index]];
+    
+//       int exclude = 0 + dp[index-1][w];
+    
+//       dp[index][w]= max(include, exclude);
+      
+//     }
+//   }
+//   return dp[n][capacity]; 
+//     }
+//  int knapSack(int capacity, vector<int> &val, vector<int> &wt) {
+//         int n=val.size()-1;
+        
+//         int ans=solveUsingTabular(capacity,n,val,wt);
+//         return ans;
+//     }
+// };
+ *//Using space optimisation
+ // as from above we need only two array one is current to save value and prev one which is to retrieve value to find include and exclude
+ class Solution {
+  public:
+    // Function to return max value that can be put in knapsack of capacity.
+    
+
+    int solveUsingSO(int capacity,int n,vector<int> &val, vector<int> &wt){
+        //index= index of last element , and we are traversing right to left
+        vector<int> prev(capacity+1,0);
+        vector<int> curr(capacity+1,0);
+        for(int w=wt[0];w<=capacity;w++){
+            if(wt[0]<=capacity){
+                prev[w]=val[0];
+            }else{
+                prev[w]=0;
+            }
+        }
+        
+    for(int index=1; index<=n; index++) {
+        for(int w=0; w<=capacity; w++) {
+          //include and exclude
+          int include = 0;
+          if(wt[index] <= w)
+            include = val[index] + prev[w - wt[index]];
+        
+          int exclude = 0 + prev[w];
+        
+          curr[w]= max(include, exclude);
+          
+        }
+        //shift
+          prev=curr;//might make mistake here
+    }
+  
+  return prev[capacity]; 
+    }
+    int knapSack(int capacity, vector<int> &val, vector<int> &wt) {
+        int n=val.size()-1;
+        
+        int ans=solveUsingSO(capacity,n,val,wt);
+        return ans;
+    }
+};
