5058304e9d5a2a3926dc52cd97ac17b07960ff08
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-11-19 12:59:49 +0530

    Update 9.*|$*Longest_Common_Subsequence.cpp

--- 16.Dynamic_Programming/9.*|$*Longest_Common_Subsequence.cpp
+++ 16.Dynamic_Programming/9.*|$*Longest_Common_Subsequence.cpp
@@ -67,3 +67,58 @@ int solveUsingMem(string&a, string&b, int i, int j,vector<vector<int>>&dp) {
 
     }
 };
+*//Solved using Tabulation
+class Solution {
+public:
+    int solveUsingTab(string a, string b) {
+        vector<vector<int> > dp(a.length()+1, vector<int>(b.length()+1, 0));
+
+        for(int i = a.length()-1; i>=0; i--) {
+            for(int j = b.length()-1; j>=0; j--) {
+                int ans = 0;
+                if(a[i] == b[j])
+                    ans =  1 + dp[i+1][j+1];
+                else {
+                    ans =  0 + max(dp[i][j+1], dp[i+1][j]);
+                }
+                dp[i][j] =  ans;
+                
+            }
+        }
+        return dp[0][0];
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        
+        return solveUsingTab(text1,text2);
+
+    }
+};
+*//space optimisation
+class Solution {
+public:
+    int solveUsingTabSO(string a, string b) {
+        //vector<vector<int> > dp(a.length()+101, vector<int>(b.length()+129, 0));
+        vector<int> curr(b.length()+1, 0);
+        vector<int> next(b.length()+1, 0);
+
+        for(int i = a.length()-1; i>=0; i--) {
+            for(int j = b.length()-1; j>=0; j--) {
+                int ans = 0;
+                if(a[i] == b[j])
+                    ans =  1 + next[j+1];
+                else {
+                    ans =  0 + max(curr[j+1], next[j]);
+                }
+                curr[j] =  ans;      
+            }
+            //shift
+            next = curr;
+        }
+        return next[0];
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        
+        return solveUsingTabSO(text1,text2);
+
+    }
+};
