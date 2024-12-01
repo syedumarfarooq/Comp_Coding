1280e6083d9168400ec1035b9edafe0574fd5c94
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-12-02 01:26:31 +0530

    Update Minimum_Cost_for_tickets.cpp

--- 16.Dynamic_Programming/Assignment/Minimum_Cost_for_tickets.cpp
+++ 16.Dynamic_Programming/Assignment/Minimum_Cost_for_tickets.cpp
@@ -86,11 +86,42 @@ public:
             j++;
         }
         int cost30=costs[2]+mincostTicketsHelperMem(days,costs,j,dp);
-        dp[i]= min(cost1,min(cost7,cost30));//here dp[i] is the minimum cost from that day until end
+        dp[i]= min(cost1,min(cost7,cost30));//here dp[i] is the minimum cost for tickets from that (ith) day until end
         return dp[i];
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         vector<int> dp(days.size(),-1);
         return mincostTicketsHelperMem(days,costs,0,dp);
+    }
+};
+*//solve using tabulation
+class Solution {
+public:
+    int mincostTicketsHelperTab(vector<int>& days,vector<int>& costs){
+        vector<int> dp(days.size()+1,-1);
+        dp[days.size()]=0;
+        for(int i=days.size()-1;i>=0;i--){
+            int cost1=costs[0]+dp[i+1];
+
+            int passEndDay=days[i]+7-1;
+            int j=i;
+            while(j<days.size()&&days[j]<=passEndDay){
+                j++;
+            }
+            int cost7=costs[1]+dp[j];
+            passEndDay=days[i]+30-1;
+            j=i;
+            while(j<days.size()&&days[j]<=passEndDay){
+                j++;
+            }
+            int cost30=costs[2]+dp[j];
+            dp[i]= min(cost1,min(cost7,cost30));//here dp[i] is the minimum cost for tickets from that (ith) day until end
+        }
+        
+        return dp[0];
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        
+        return mincostTicketsHelperTab(days,costs);
     }
 };
