8af3ccabaf30d4c5869b98304099cd8dba2e0d4f
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-12-28 01:43:30 +0530

    Update 14.Bellman_Ford_Algo.cpp

--- 17.Graphs/14.Bellman_Ford_Algo.cpp
+++ 17.Graphs/14.Bellman_Ford_Algo.cpp
@@ -97,23 +97,23 @@ void bellmanFordAlgo(int n, int src) {//TC:O(nE)//n-1 relaxation for all edges
 		
 	}
 };
-int main() {
-	graph g;
+// int main() {
+// 	graph g;
 
-	g.addEdge(0,1,-1,0);
-	g.addEdge(0,2,4,0);
-	g.addEdge(1,2,3,0);
+// 	g.addEdge(0,1,-1,0);
+// 	g.addEdge(0,2,4,0);
+// 	g.addEdge(1,2,3,0);
 
-	g.addEdge(1,3,2,0);
-	g.addEdge(1,4,2,0);
-	g.addEdge(3,1,1,0);
-	g.addEdge(3,2,5,0);
-	g.addEdge(4,3,-3,0);
+// 	g.addEdge(1,3,2,0);
+// 	g.addEdge(1,4,2,0);
+// 	g.addEdge(3,1,1,0);
+// 	g.addEdge(3,2,5,0);
+// 	g.addEdge(4,3,-3,0);
 	
-	////////////
+// 	////////////
 
-	g.printAdjList();
-	g.bellmanFordAlgo(5, 0);
+// 	g.printAdjList();
+// 	g.bellmanFordAlgo(5, 0);
 
-	return 0;
-}
+// 	return 0;
+// }
