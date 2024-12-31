5f84f0f4bf617a2ad924423cd7b13b571dacdbb7
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-12-31 12:56:51 +0530

    Create 17.Bridge_in_a_graph.cpp

---
+++ 17.Graphs/17.Bridge_in_a_graph.cpp
@@ -0,0 +1,3 @@
+*//Bridge in a graph using tarjan's algorithm
+// Bridge is an edge in a graph on removing it will increase the count of number of disconnected component
+// eg:a-b if we remove edge a-b then the number of disconnected component becomes 2 which is a and b
