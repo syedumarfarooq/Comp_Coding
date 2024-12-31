1c18627a00041f3575ec4cba57cf91133ba8469d
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2024-12-31 11:47:34 +0530

    Update 13.Graphs.cpp

--- 17.Graphs/13.Graphs.cpp
+++ 17.Graphs/13.Graphs.cpp
@@ -7,3 +7,15 @@ eg:1-2-3-1 where wheight from (1-2):-2,(2-3):-3,(3-1):-4
   // undirected graph
   *//dijkstra will not work on negative cycle and negative weight(this will be taught later by taking some cases where it fails)
   //until then it is working perfectly for negative weights directed graph eg;1-2-3 1-3 (1-2):5 (2-3):-10 (1-3):2
+*//dijkstra can be implemented in many ways(3) but in our solution we have used priority queue or set in that re-evalution is allowed
+  // so there are 2 ways to implement priority queue method
+  // 1.with re-evaluation
+    // this will not work for undirected graph with negative weights or negative cycle in both directed and undirected but will work 
+    // fine with negative weights in directed graph
+  // 2.without re-evaluation
+    // in this it wont work for negative cycles or negative weights as we cannot update the distance again once it ks updateed it cannot
+    //be changed
+  // *//dijkstra can be implemented in 3 ways
+  // 1.Nested for loops
+  // 2.priority queue/set and re-evalution not allowed
+  // 3.priority queue/set and re-evalution is allowed
