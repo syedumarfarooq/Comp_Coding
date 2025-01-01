e649afef13ea01d6e145fdc45e30ea3a3e518663
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2025-01-01 19:37:23 +0530

    Update Minimum_Multiplications_to_reach_end.cpp

--- 17.Graphs/Assignment/Minimum_Multiplications_to_reach_end.cpp
+++ 17.Graphs/Assignment/Minimum_Multiplications_to_reach_end.cpp
@@ -1,4 +1,5 @@
 https://www.geeksforgeeks.org/problems/minimum-multiplications-to-reach-end/0
+*//my guess coin change can be solve using this method
 // Given start, end and an array arr of n numbers. At each step, start is multiplied with any number in the array and then mod operation with 100000 is done to get the new start.
 
 // Your task is to find the minimum steps in which end can be achieved starting from start. If it is not possible to reach end, then return -1.
