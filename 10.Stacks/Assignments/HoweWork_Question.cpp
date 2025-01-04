521a59e7d5480e53d48eb66ffe8aa85d4ed12e24
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2025-01-05 03:15:09 +0530

    Create HoweWork_Question.cpp

---
+++ 10.Stacks/Assignments/HoweWork_Question.cpp
@@ -0,0 +1,13 @@
+Below you can find the list of Homework questions :
+• Minimum Bracket Reversal
+• Remove All Adjacent Duplicates In String
+• Celebrity Problem
+• Next greater element in Linked List [leetcode]
+• N Stacks in an Array
+• Online Stock Span [Leetcode]
+• Check If Word Is Valid After Substitutions [Leetcodel
+• Decode Strings [Leetcode]
+• Car Fleet - I [Leetcode]
+• Car Fleet - II [Leetcode]
+• Simplify Path [Leetcode]
+• Max rectangle in Binary Matrix with all 1s
