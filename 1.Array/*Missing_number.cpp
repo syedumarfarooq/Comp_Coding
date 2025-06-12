f5020a786e52d8b3d9c7293998f5c69a0653a7a2
Syed Umar Farooq <115428506+syedumarfarooq@users.noreply.github.com>
2025-06-13 00:03:04 +0530

    Update *Missing_number.cpp

--- 1.Array/*Missing_number.cpp
+++ 1.Array/*Missing_number.cpp
@@ -18,12 +18,20 @@ https://leetcode.com/problems/missing-number/
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int res = nums.size();
-        
+        int res = nums.size();// adding last index to the result becuase to find the sum of indexes we need all the index
+        //but as we are considering the array for index there is one less index which is the last index so we add it
+        //as there is one element missing in the array so the array will be smaller by one element so we add nums.size()
+        //eg: arr:0,1,3 index:0,1,2 so for the sum of index should be 0+1+2+3 to find the difference and the missing element
+        //0+1+2+3 - (0+1+3)(array sum) will give us the missing element
+        // Because in a list of size n, the numbers should be from 0 to n. So:
+        //     The indexes go from 0 to n-1
+        //     But the numbers are from 0 to n (total n+1 numbers)
+            // so n is missing
         for (int i = 0; i < nums.size(); i++) {
-            res += i - nums[i];
+            res += i - nums[i];// sum of indexes= sum of elements in array if there are all the elements present in the array
         }
-        
+        //so sum of indexes- sum of elements will give us 0 if all the elements are present
+        //if there is any missing number  sum of indexes- sum of elements will give us that number
         return res;        
     }
 };
